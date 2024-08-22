0214534b10eb8309a1c744958296b71c95a52e79
destx0 <destisdbest@gmail.com>
2024-08-22 11:00:23 +0530

    punu bvook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import QuizComponent from "@/components/QuizComponent";
 export default function Home() {
 	return (
 		<div className="container mx-auto p-4">
-			<h1 className="text-3xl font-bold underline mb-4">Hello world!</h1>
+			<h1 className="text-3xl font-bold underline mb-4">Punu Book</h1>
 			<QuizComponent />
 		</div>
 	);
