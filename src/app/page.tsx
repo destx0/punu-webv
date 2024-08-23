8540d08719de9508d1bdbb1ebb8d1120229aa7fa
destx0 <destisdbest@gmail.com>
2024-08-23 11:07:07 +0530

    padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import QuizComponent from "@/components/QuizComponent";
 
 export default function Home() {
 	return (
-		<div className="container mx-auto p-4">
-			<h1 className="text-3xl font-bold underline mb-4">Punu Book</h1>
+		<div className="container mx-auto px-4">
 			<QuizComponent />
 		</div>
 	);
